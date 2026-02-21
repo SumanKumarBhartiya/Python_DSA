bb0e5563ac0e65b8ca5d5502fb0049e0109af594
Suman Bhartiya <webdevvikky@gmail.com>
2026-02-21 19:33:49 +0530

    code improvement for merge sorted arr

--- merge_sorted_arr.py
+++ merge_sorted_arr.py
@@ -9,24 +9,25 @@ def merge_sorted_arr(arr1, m, arr2, n):
         arr_1_val = arr1[arr_1_ptr]
         arr_2_val = arr2[arr_2_ptr]
 
-        if arr_1_val < arr_2_val and arr_1_val != 0:
+        if arr_1_val < arr_2_val:
             ret_list.append(arr_1_val)
             arr_1_ptr +=1
-        elif arr_1_val > arr_2_val and arr_2_val != 0:
+        # elif arr_1_val > arr_2_val:
+        else:
             ret_list.append(arr_2_val)
             arr_2_ptr +=1
-        elif arr_1_val == arr_2_val and arr_2_val != 0:
-            ret_list.append(arr_1_val)
-            ret_list.append(arr_2_val)
-            arr_1_ptr +=1
-            arr_2_ptr +=1
+        # elif arr_1_val == arr_2_val:
+        #     ret_list.append(arr_1_val)
+        #     ret_list.append(arr_2_val)
+        #     arr_1_ptr +=1
+        #     arr_2_ptr +=1
 
-    for i in range(arr_1_ptr, len(arr1)):
-        if arr1[i] != 0:
-            ret_list.append(arr1[i])
-    for i in range(arr_2_ptr, len(arr2)):
-        if arr2[i] != 0:
-            ret_list.append(arr2[i])
+    while arr_1_ptr <  m:
+        ret_list.append(arr1[arr_1_ptr])
+        arr_1_ptr+=1
+    while arr_2_ptr <  n:
+        ret_list.append(arr2[arr_2_ptr])
+        arr_2_ptr+=1
 
     return ret_list
 
