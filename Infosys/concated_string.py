483670178b2e0dad3055eea4b0bde6c146231193
Suman Bhartiya <skb08725@gmail.com>
2026-07-14 09:35:20 +0530

    fix : concated_string

--- Infosys/concated_string.py
+++ Infosys/concated_string.py
@@ -17,3 +17,21 @@ def concated_string(str1: str, str2 : str):
 
 print(concated_string("aacdb", "gafd"))
 print(concated_string("abcs", "cxzca"))
+
+def concatenatedString(s1: str, s2: str) -> str:
+    res = ""
+    s = set(s2)
+
+    for c in s1:
+        if c not in s:
+            res += c
+        else:
+            s.remove(c)
+
+    res += "".join(s)
+
+    return res
+
+s1 = "abcs"
+s2 = "cxzca"
+print(concatenatedString(s1, s2)) # Output: bsxz
